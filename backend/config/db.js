724fc21f33fcddb482950c28da5c7ebdca9582f1
Smit Modi <98140609+MojoSmit07@users.noreply.github.com>
2024-07-23 17:52:27 +0530

    Chnages to Host backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
-export const  connectDB = async () =>{
-
-    await mongoose.connect('{mongodb+srv://FoodRush:FoodRush@foodrush.l9kpb31.mongodb.net/?retryWrites=true&w=majority&appName=FoodRush/food-del').then(()=>console.log("DB Connected"));
-   
-}
-
-
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
+export const connectDB = async () => {
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("DB Connected"))
+    .catch((error) => console.log("DB Connection Error:", error));
+};
